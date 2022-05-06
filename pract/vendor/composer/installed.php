66845147a355b12bdf521e036b1d460e8dc8e1ad
azlk2140 <ian.kachuk@mail.ru>
2022-05-06 16:18:04 +0600

    Пеервый коммит

---
+++ pract/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => NULL,
+        'name' => 'user/moskvich2',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'user/moskvich2' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => NULL,
+            'dev_requirement' => false,
+        ),
+    ),
+);
